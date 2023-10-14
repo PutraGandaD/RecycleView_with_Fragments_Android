b1c4a13cb68a9236a9a6c43e73b595dab54892c1
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2023-10-15 00:26:05 +0700

    Successfully implement RecyclerView inside HomeFragment

--- app/src/main/java/com/putragandad/rvfragment/HomeFragment.kt
+++ app/src/main/java/com/putragandad/rvfragment/HomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,8 +19,11 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class HomeFragment : Fragment() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
     }
 
     override fun onCreateView(
@@ -27,5 +32,26 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var recyclerView : RecyclerView = view.findViewById(R.id.rv_movielist)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
+        val movieList = ArrayList<MovieList>()
+
+        movieList.add(MovieList(R.drawable.movie_1, "Mission: Impossible Dead Reckoning Part 1", "2023", "Tom Cruise, Rebecca Ferguson, Hayley Atwell"))
+        movieList.add(MovieList(R.drawable.movie_2, "Titanic", "1997", "Leonardo DiCaprio, Kate Winslet, etc"))
+        movieList.add(MovieList(R.drawable.movie_3, "The Last of Us", "2023", "Pedro Pascal, Bella Ramsey, Anna Torv, etc"))
+        movieList.add(MovieList(R.drawable.movie_4, "Avatar: The Way of Water", "2022", "Sam Worthington, Zoe Saldana, Sigourney Weaver, etc"))
+
+
+
+        val movieListAdapter = MovieListAdapter(movieList)
+
+        recyclerView.adapter = movieListAdapter
     }
 }
