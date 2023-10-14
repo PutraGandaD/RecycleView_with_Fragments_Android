292077fa9a25d483f95bd2e056cb599966444612
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2023-10-14 22:13:24 +0700

    Added Material Navigation Bar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.putragandad.rvfragment"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.putragandad.rvfragment"
