71224e4786647d0d6591b39bca4dee65011f285e
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2023-10-14 22:29:40 +0700

    Added Home/Saved Fragment

--- app/src/main/java/com/putragandad/rvfragment/MainActivity.kt
+++ app/src/main/java/com/putragandad/rvfragment/MainActivity.kt
@@ -2,10 +2,43 @@ package com.putragandad.rvfragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.putragandad.rvfragment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        replaceFragment(HomeFragment())
+
+        binding.navbar.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.home -> {
+                    replaceFragment(HomeFragment())
+                }
+                R.id.bookmark -> {
+                    replaceFragment(SavedFragment())
+                }
+                else -> {
+
+                }
+            }
+
+            true
+        }
+    }
+
+    private fun replaceFragment (fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
     }
 }
